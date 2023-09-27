eb5cdd5f19355141ba5e7e17645b3eb9ef108262
samhi309 <143259019+samhi309@users.noreply.github.com>
2023-09-27 10:44:01 +0530

    Create StaticVariableDemo.java

---
+++ StaticVariableDemo.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class Student{
+    int rollno;
+    String name;
+    static String college="SITE";
+Student(int r,String n){
+rollno=r;
+name=n;
+}
+void display(){
+    System.out.println(rollno+" "+name+" "+college+" ");
+}
+}
+public class StaticVariableDemo{
+    public static void main(String args[]){
+        Student s1=new Student(222,"samhi");
+        Student s2=new Student(215,"divya");
+        System.out.println("Student details are : ");
+        s1.display();
+        s2.display();
+    }
+}
